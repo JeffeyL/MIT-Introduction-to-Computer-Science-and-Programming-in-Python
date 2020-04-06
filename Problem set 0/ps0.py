ae16d726a57d7355d97748b549b8e89b701b6af4
JeffeyL <49671320+JeffeyL@users.noreply.github.com>
2020-04-05 17:15:34 -0700

    Create ps0.py

---
+++ Problem set 0/ps0.py
@@ -0,0 +1,10 @@
+import math
+
+x = float(input("Enter number x: "))
+y = float(input("Enter number y: "))
+
+power = int(math.pow(x, y))
+print("x**y = " + str(power))
+
+logx = int(math.log(x, 2))
+print("log(x) = " + str(logx))
